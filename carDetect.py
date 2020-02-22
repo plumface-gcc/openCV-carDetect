d9cc9a47c35d9c29d9b295fb3e9b275a2c84159a
time007gc <200422828@student.georgianc.on.ca>
2020-02-22 03:32:01 -0500

    Add main code and haar cascade

---
+++ carDetect.py
@@ -0,0 +1,38 @@
+import numpy as np
+import cv2
+import urllib.request
+
+car_cascade = cv2.CascadeClassifier("car.xml")
+
+url = "http://192.168.1.14:8080/shot.jpg"
+
+cap = cv2.VideoCapture()
+
+def phonecamera():
+    while True:
+
+        ret, img = cap.read()  # read frame by frame
+        img_arr = np.array(bytearray(urllib.request.urlopen(url).read()), dtype=np.uint8) # create a byte array that opens and reads a url
+        img = cv2.imdecode(img_arr, -1)
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        car_multiScale = car_cascade.detectMultiScale(gray, 1.10, 5) # self, layers, K neighbors
+
+        for (x, y, w, h) in car_multiScale:
+            cv2.putText(img, "Car", (x, y - 20), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 3)
+
+            cv2.rectangle(img, (x, y), (x + y, y + h), (255, 0, 0), 2) # draw box to screen
+
+        cv2.imshow('IPWebcam Output', img) # output image
+
+        key = cv2.waitKey(35) & 0xff
+        if key == 27:
+            break
+
+def mainrun():
+    try:
+        phonecamera()
+    except urllib.error.URLError as e:
+        print("Bad url entered. Please run program again with new args.")
+
+mainrun()
